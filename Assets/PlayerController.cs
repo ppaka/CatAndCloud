c83a5b13fa24b73d7fd99919164ea2a668ca13e1
ppaka <asj0216@gmail.com>
2022-06-17 14:28:17 +0900

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigid2D;
+    Animator animator;
+    float jumpForce = 600;
+    float walkForce = 30;
+
+    float maxWalkSpeed = 2;
+
+    private void Start()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid2D.AddForce(transform.up * jumpForce);
+        }
+
+        var key = 0;
+
+        if (Input.GetKey(KeyCode.RightArrow)) key = 1;
+        if (Input.GetKey(KeyCode.LeftArrow)) key = -1;
+
+        var speedx = Mathf.Abs(rigid2D.velocity.x);
+        if (speedx < maxWalkSpeed)
+        {
+            rigid2D.AddForce(transform.right * key * walkForce);
+        }
+        if (key != 0)
+        {
+            transform.localScale = new Vector3(key, 1, 1);
+        }
+        animator.speed = speedx * 0.5f;
+    }
+}
